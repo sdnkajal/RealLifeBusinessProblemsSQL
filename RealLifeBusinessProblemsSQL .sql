d821592ccaad7b7b947cbe64a3386aaf43aab4d7
sdnkajal <145316443+sdnkajal@users.noreply.github.com>
2023-11-05 13:50:58 +0600

    Update RealLifeBusinessProblemsSQL .sql

--- RealLifeBusinessProblemsSQL .sql
+++ RealLifeBusinessProblemsSQL .sql
@@ -1,29 +1,29 @@
 -- Task 01
 SELECT
-c.customer_id, c.name, c.email, c.location, COUNT(o.order_id) AS total_orders
-FROM Customers c LEFT JOIN Orders o ON c.customer_id = o.customer_id
-GROUP BY c.customer_id, c.name, c.email, c.location
+c.id, c.name, c.email, c.location, COUNT(o.id) AS total_orders
+FROM customers c LEFT JOIN orders o ON c.id = o.customer_id
+GROUP BY c.id, c.name, c.email, c.location
 ORDER BY total_orders DESC;
 
 -- Task 02
 SELECT
-o.order_id, p.name AS product_name, oi.quantity, oi.quantity * oi.unit_price AS total_amount
-FROM Order_Items oi JOIN Products p ON oi.product_id = p.product_id
-ORDER BY o.order_id ASC;
+oi.id, p.name AS product_name, oi.quantity, oi.quantity * oi.price AS total_amount
+FROM order_items oi JOIN Products p ON oi.product_id = p.id
+ORDER BY oi.id ASC;
 
 -- Task 03
 SELECT
-c.name AS category_name, SUM(oi.quantity * oi.unit_price) AS total_revenue
-FROM Categories c
-JOIN Products p ON c.category_id = p.category_id
-JOIN Order_Items oi ON p.product_id = oi.product_id
+c.name AS category_name, SUM(oi.quantity * oi.price) AS total_revenue
+FROM categories c
+JOIN products p ON c.id = p.categorie_id
+JOIN order_items oi ON p.id = oi.product_id
 GROUP BY c.name ORDER BY total_revenue DESC;
 
 -- Task 04
 SELECT
-c.name AS customer_name, SUM(oi.quantity * oi.unit_price) AS total_purchase_amount
-FROM Customers c
-JOIN Orders o ON c.customer_id = o.customer_id
-JOIN Order_Items oi ON o.order_id = oi.order_id
+c.name AS customer_name, SUM(oi.quantity * oi.price) AS total_purchase_amount
+FROM customers c
+JOIN Orders o ON c.id = o.customer_id
+JOIN Order_Items oi ON o.id = oi.order_id
 GROUP BY c.name ORDER BY total_purchase_amount DESC
 LIMIT 5;
